9248e975c744369f7071d6391890092a60b7ad23
JuliePadilla <julie,andrea.padilla@gmail.com>
2021-08-16 16:11:28 -0500

    third commit

--- src/components/Request.vue
+++ src/components/Request.vue
@@ -1,5 +1,44 @@
 <template>
     <div id="Request">
+        <div class="Encabezado">
+            <img src="../assets/quiero_saber.png">
+            <h1> | </h1>
+            <span class="QUIERO-SABER">QUIERO SABER</span>
+        </div>
+        <div class="Contenido">
+            <div class="Lineamientos">
+                <div class="titulo">
+                    <img src="../assets/lineamientos.png">
+                    <p>LINEAMIENTOS</p>
+                </div>
+                <div class="Lnea-4"></div>
+                <div class="texto">
+                    <h1>politicas xxxx xsfdxgcfvgbhn tcvgtcvt etrdytf restdf restdrf
+                    zrxtryctu vyiukoij uhgfyugyihoj pkoijuygtrd ytugiiuyvcvbn bvcyvuybnb
+                    dfgh fyguhj ytughji ytguhji rfytguhij yftguhji rtfyguhj rfytg</h1>
+                </div>
+            </div>
+            
+            <div class="Socializacion">
+                <div class="titulo">
+                </div>
+            </div>
+
+            <div class="Fortalecimiento">
+                <div class="titulo">
+                </div>
+            </div>
+
+            <div class="Acompanamiento">
+                <div class="titulo">
+                </div>
+            </div>
+
+            <div class="Medicion">
+                <div class="titulo">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -7,5 +46,132 @@
 </script>
 
 <style>
+#Request .Contenido {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+#Request .Encabezado {
+    display: flex;
+    justify-content: center;
+    padding-bottom: 20px;
+}
+#Request .Encabezado h1{
+    font-size: 50px;
+    font-weight: lighter;
+}
+#Request .Encabezado img {
+  width: 69px;
+  height: 75px;
+  margin: 0 11px 0 0;
+}
+#Request .QUIERO-SABER {
+  width: 202px;
+  height: 30px;
+  margin: 23px 0 22px 11px;
+  font-family: Montserrat;
+  font-size: 25px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #050505;
+}
+#Request .Lineamientos {
+  width: 509px;
+  height: 160px;
+  margin: 10px 10px 10px 100px;
+  padding: 0px 30px 42px 35px;
+  border-radius: 20px;
+  border: solid 1px #3366cc;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+}
+#Request .Lineamientos img {
+  width: 92px;
+  height:90px;
+  margin: 10px 10px 10px 25px;/*top-right-bottom-left*/
+}
+#Request .Lineamientos .Lnea-4 {
+  width: 0;
+  height: 159px;
+  margin: 0px 0 43.5px;/*top-right-bottom-left*/
+  border: solid 1px #3366cc;
+}
+#Request .Lineamientos p {
+  width: 161px;
+  height: 24px;
+  margin: 14px 20px 0 0;/*top-right-bottom-left*/
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #36c;
+}
+#Request .Lineamientos h1 {
+  width: 226px;
+  height: 162px;
+  margin: 10px 10px 0 10px;/*top-right-bottom-left*/
+  font-family: Montserrat;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.29;
+  letter-spacing: normal;
+  text-align: left;
+  color: #36c;
+}
+#Request .Socializacion {
+  width: 509px;
+  height: 160px;
+  margin: 10px 100px 10px 10px;/*top-right-bottom-left*/
+  padding: 28px 155px 42px 35px;
+  border-radius: 20px;
+  border: solid 1px #3366cc;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+}
+#Request .Fortalecimiento {
+  width: 509px;
+  height: 160px;
+  margin: 10px 10px 10px 100px;
+  padding: 28px 30px 42px 35px;
+  border-radius: 20px;
+  border: solid 1px #3366cc;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+}
+#Request .Acompanamiento {
+  width: 509px;
+  height: 160px;
+  margin: 10px 100px 10px 10px;
+  padding: 28px 155px 42px 35px;
+  border-radius: 20px;
+  border: solid 1px #3366cc;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+}
+#Request .Medicion {
+  width: 509px;
+  height: 160px;
+  margin: 10px 10px 10px 100px;
+  padding: 28px 155px 42px 35px;
+  border-radius: 20px;
+  border: solid 1px #3366cc;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+}
 </style>
        
